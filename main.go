f98a31ccfcd503ca6e88b436bce344b5d3edc6f7
Fernando Paiva <fernandopaiva@techamazon.tech>
2022-12-16 13:19:42 -0300

    adiciona configuração de CORS

--- main.go
+++ main.go
@@ -4,35 +4,35 @@ import (
 	"risqlac-api/controllers"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
-type Response struct {
-	Message string `json:"message"`
-}
-
 func main() {
 	app := fiber.New()
+
 	app.Use(logger.New())
 	app.Use(requestid.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	app.Get("/info", func(context *fiber.Ctx) error {
-		return context.Status(fiber.StatusOK).JSON(Response{
-			Message: "RisQLAC API",
-		})
+		return context.Status(fiber.StatusOK).SendString("RisQLAC API v1.0")
 	})
 
-	user := app.Group("/user")
-	product := app.Group("/product")
+	userRoutes := app.Group("/user")
+	productRoutes := app.Group("/product")
 
-	user.Post("/create", controllers.CreateUser)
-	user.Get("/list", controllers.ListUsers)
-	user.Delete("/delete", controllers.DeleteUser)
+	userRoutes.Post("/create", controllers.CreateUser)
+	userRoutes.Get("/list", controllers.ListUsers)
+	userRoutes.Delete("/delete", controllers.DeleteUser)
 
-	product.Post("/create", controllers.CreateProduct)
-	product.Get("/list", controllers.ListProducts)
-	product.Delete("/delete", controllers.DeleteProduct)
+	productRoutes.Post("/create", controllers.CreateProduct)
+	productRoutes.Get("/list", controllers.ListProducts)
+	productRoutes.Delete("/delete", controllers.DeleteProduct)
 
 	app.Listen(":3000")
 }
