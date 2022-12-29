fcb35ab1fedc5c0e2e53473ce38f93d2de10d441
Fernando <fernandopaivaec@gmail.com>
2022-12-28 23:14:36 -0300

    as variáveis de ambiente não são mais lidas do arquivo .env

--- environment/load.go
+++ environment/load.go
@@ -1,23 +1,14 @@
 package environment
 
 import (
-	"os"
 	"risqlac-api/types"
-
-	"github.com/joho/godotenv"
 )
 
 var env types.Env
 
 func Load() {
-	err := godotenv.Load()
-
-	if err != nil {
-		panic("Error loading environment variables")
-	}
-
-	env.JWT_SECRET = os.Getenv("JWT_SECRET")
-	env.DATABASE_FILE = os.Getenv("DATABASE_FILE")
+	env.JWT_SECRET = "0293rjdik023o3uihfdrca2q0o938hrfcd902382c9epjrfcd0239jrf"
+	env.DATABASE_FILE = "risqlac.db"
 }
 
 func Get() types.Env {
--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.19
 require (
 	github.com/gofiber/fiber/v2 v2.40.1
 	github.com/golang-jwt/jwt/v4 v4.4.3
-	github.com/joho/godotenv v1.4.0
 	golang.org/x/crypto v0.4.0
 	gorm.io/driver/sqlite v1.4.3
 	gorm.io/gorm v1.24.2
