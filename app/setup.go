623907c279f385e18928b09aaa325c8ed93e2cda
Fernando Paiva <fernandopaiva@techamazon.tech>
2023-02-15 13:21:47 -0300

    atualiza dependências

--- app/setup.go
+++ app/setup.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
@@ -11,16 +12,16 @@ var Instance *fiber.App
 
 func Setup() {
 	Instance = fiber.New()
-
 	Instance.Use(logger.New())
 	Instance.Use(requestid.New())
-
+	Instance.Use(compress.New(compress.Config{
+		Level: compress.LevelBestSpeed,
+	}))
 	Instance.Use(cors.New(cors.Config{
 		AllowOrigins: "*",
 		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
-
 	Instance.Get("/info", func(context *fiber.Ctx) error {
-		return context.Status(fiber.StatusOK).SendString("RisQLAC API v2.2.11")
+		return context.Status(fiber.StatusOK).SendString("RisQLAC API v2.2.12")
 	})
 }
--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.4.3
 	github.com/joho/godotenv v1.5.1
 	github.com/sendgrid/sendgrid-go v3.12.0+incompatible
-	golang.org/x/crypto v0.5.0
+	golang.org/x/crypto v0.6.0
 	gorm.io/driver/sqlite v1.4.4
 	gorm.io/gorm v1.24.5
 )
@@ -40,5 +40,5 @@ require (
 	github.com/valyala/fasthttp v1.44.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 )
