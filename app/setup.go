406700d4082603dd0df3c213197dbbb3de96b576
Fernando Paiva <fernandopaiva@techamazon.tech>
2023-02-15 15:54:23 -0300

    adiciona middleware de recuperação de erros e rota para exibir métricas de monitoramento

--- app/setup.go
+++ app/setup.go
@@ -1,10 +1,14 @@
 package app
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
@@ -12,16 +16,25 @@ var Instance *fiber.App
 
 func Setup() {
 	Instance = fiber.New()
+
+	Instance.Use(recover.New())
 	Instance.Use(logger.New())
 	Instance.Use(requestid.New())
 	Instance.Use(compress.New(compress.Config{
 		Level: compress.LevelBestSpeed,
 	}))
+
 	Instance.Use(cors.New(cors.Config{
 		AllowOrigins: "*",
 		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
+
 	Instance.Get("/info", func(context *fiber.Ctx) error {
-		return context.Status(fiber.StatusOK).SendString("RisQLAC API v2.2.12")
+		return context.Status(fiber.StatusOK).SendString("RisQLAC API v2.2.13")
 	})
+
+	Instance.Get("/metrics", monitor.New(monitor.Config{
+		Title:   "RisQLAC API Metrics",
+		Refresh: time.Second * 5,
+	}))
 }
