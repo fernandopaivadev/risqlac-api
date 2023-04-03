7a57d45fe43fbf25f261d427d9d12f8cb8b10bdb
Fernando Paiva <fernandopaiva@techamazon.tech>
2023-04-03 18:43:50 -0300

    remove função skipper do middleware de compressão

--- application/server.go
+++ application/server.go
@@ -22,22 +22,6 @@ func (server *server) Setup() {
 	server.Instance.Use(echoMiddleware.RequestID())
 	server.Instance.Use(echoMiddleware.GzipWithConfig(echoMiddleware.GzipConfig{
 		Level: 5,
-		Skipper: func(context echo.Context) bool {
-			path := context.Path()
-
-			switch path {
-			case "/product/report/xlsx":
-				return true
-			case "/product/report/pdf":
-				return true
-			case "/product/report/csv":
-				return true
-			case "/product/report/json":
-				return true
-			default:
-				return false
-			}
-		},
 	}))
 	server.Instance.Use(echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
 		AllowOrigins: []string{"*"},
